0b1ee1db2ebbf7f93f89e280b5ecbd7be5875ce1
nelogeek <lokotaevpr@gmail.com>
2023-08-24 16:44:54 +0500

    Добавьте файлы проекта.

---
+++ Parser (rubilnik.ru)/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Parser__rubilnik.ru_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 37);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(409, 96);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "https://rubilnik.ru/catalog/kabelno_provodnikovaya_produktsiya/kabeli_i_provoda_s" +
+    "vyazi_1/kabel_pozharnoy_signalizatsii_1/";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(299, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Обновить данные";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 139);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Выбрать Excel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ссылка на страницу с товарами";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 174);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.MaximumSize = new System.Drawing.Size(450, 213);
+            this.MinimumSize = new System.Drawing.Size(450, 213);
+            this.Name = "Form1";
+            this.Text = "Parser (rubilnik.ru)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Parser (rubilnik.ru)/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net.Http;
+using HtmlAgilityPack;
+using OfficeOpenXml;
+using System.IO;
+
+
+namespace Parser__rubilnik.ru_
+{
+    public partial class Form1 : Form
+    {
+        string excelFilePath = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Вставьте ссылку на страницу с товаром");
+                return;
+            }
+
+            await ProcessParsingAsync(textBox1.Text, excelFilePath);
+        }
+
+        private async Task ProcessParsingAsync(string url, string excelFilePath)
+        {
+            try
+            {
+                Disable();
+                await ParseAndSaveAsync(url, excelFilePath);
+                Enable();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка: {ex.Message}");
+            }
+        }
+
+        private void Enable()
+        {
+            textBox1.Enabled = true;
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+
+        private void Disable()
+        {
+            textBox1.Enabled = false;
+            button1.Enabled = false;
+            button2.Enabled = false;
+        }
+
+        private async Task ParseAndSaveAsync(string url, string excelFilePath)
+        {
+            try
+            {
+                ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+
+                using (var package = new ExcelPackage(new FileInfo(excelFilePath)))
+                {
+                    var worksheet = package.Workbook.Worksheets[0];
+                    var web = new HtmlWeb();
+                    var htmlDocument = await web.LoadFromWebAsync(url);
+
+                    var blockNodes = htmlDocument.DocumentNode.SelectNodes("//div[@class='inner_wrap TYPE_1']");
+                    if (blockNodes != null)
+                    {
+                        foreach (var blockNode in blockNodes)
+                        {
+                            var titleNode = blockNode.SelectSingleNode(".//div[@class='item-title']");
+                            var priceNode = blockNode.SelectSingleNode(".//span[@class='price_value']");
+
+                            if (titleNode != null && priceNode != null)
+                            {
+                                string title = titleNode.InnerText.Trim();
+                                string price = priceNode.InnerText.Trim();
+
+                                var row = worksheet.Cells["A:A"].FirstOrDefault(cell => cell.Value?.ToString() == title)?.Start.Row;
+
+                                if (row.HasValue)
+                                {
+                                    worksheet.Cells[row.Value, 3].Value = price;
+                                }
+                            }
+                        }
+
+                        await package.SaveAsync();
+                    }
+                }
+                MessageBox.Show("Парсинг завершен успешно.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " +  ex.Message + "\n\n\n\n P.s. если у вас открыт файл Excel, который вы пытаетесь обновить, то необходимо его закрыть, чтобы избежать неправильной работы программы", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+            openFileDialog.Title = "Select an Excel File";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                excelFilePath = openFileDialog.FileName;
+            }
+
+            if (excelFilePath != null)
+            {
+                button1.Enabled = true;
+            }
+        }
+
+
+    }
+}
+
+
+//private void button1_Click(object sender, EventArgs e)
+//{
+//    string url = textBox1.Text;
+//    parse(url);
+//}
+//
+//async private void parse(string url)
+//{
+//    // Создание экземпляра HttpClient для выполнения HTTP-запросов
+//    using (var httpClient = new HttpClient())
+//    {
+//        // Выполнение GET-запроса и получение содержимого страницы
+//        string html = await httpClient.GetStringAsync(url);
+//
+//        // Создание экземпляра HtmlDocument для обработки HTML
+//        var htmlDocument = new HtmlAgilityPack.HtmlDocument();
+//        htmlDocument.LoadHtml(html);
+//
+//        // Используем XPath для нахождения блоков класса "inner_wrap TYPE_1"
+//        var blockNodes = htmlDocument.DocumentNode.SelectNodes("//div[@class='inner_wrap TYPE_1']");
+//        if (blockNodes != null)
+//        {
+//            foreach (var blockNode in blockNodes)
+//            {
+//                // Находим элементы внутри блока
+//                var titleNode = blockNode.SelectSingleNode(".//div[@class='item-title']");
+//                var valueNode = blockNode.SelectSingleNode(".//span[@class='value font_sxs']");
+//                var priceNode = blockNode.SelectSingleNode(".//span[@class='price_value']");
+//
+//                Console.WriteLine();
+//
+//                // Извлекаем текст из элементов и выводим на экран
+//                if (titleNode != null)
+//                {
+//                    Console.WriteLine("Title: " + titleNode.InnerText.Trim());
+//                }
+//                if (valueNode != null)
+//                {
+//                    Console.WriteLine("Value: " + valueNode.InnerText.Trim());
+//                }
+//                if (priceNode != null)
+//                {
+//                    Console.WriteLine("Price: " + priceNode.InnerText.Trim());
+//                }
+//
+//
+//            }
+//        }
+//    }
+//}
